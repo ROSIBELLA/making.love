dc4a6ccbaad4694c0061f35adc9fb9e9e8107d8c
Camilo Sanchez <daniel.sanchez24102001@gmail.com>
2020-10-31 23:58:45 -0500

    hola

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
+const box = document.getElementById('container2');
+const box2 = document.getElementById('heart-box');
+
 function sayHi(){
    alert('TE QUIERO MUCHOOOO NIÑA FEA \nME ENCANTAAAS!!!')
-   location.href ="../html/finish.html";
+   // box.classList.remove('desactive');
+   box.classList.add('desactive');
+   box2.classList.add('active');
 }
